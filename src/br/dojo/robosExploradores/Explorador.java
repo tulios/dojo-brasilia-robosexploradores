9ce84564d9a37ee510d9ff693721b616bf0e2d63
unknown <ornelas.tulio@gmail.com>
2009-01-17 14:05:14 -0200

    [Tulio e Celio] -> Gerou a classe exploradora que define o tamanho do mapa.

---
+++ src/br/dojo/robosExploradores/Explorador.java
@@ -0,0 +1,19 @@
+package br.dojo.robosExploradores;
+
+public class Explorador {
+	private int linha;
+	private int coluna;
+
+	public int getLinha() {
+		return linha;
+	}
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	public int getColuna() {
+		return coluna;
+	}
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+}
--- test/br/dojo/robosExploradores/TesteLeituraMapa.java
+++ test/br/dojo/robosExploradores/TesteLeituraMapa.java
@@ -1,11 +1,67 @@
 package br.dojo.robosExploradores;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TesteLeituraMapa {
 	
 	@Test
-	public void init(){
+	public void verificaLeituraTamanhoMapa(){
+		Explorador explorador = new Explorador();
+		explorador.setLinha(4);
+		explorador.setColuna(4);
+		
+		assertEquals(4, explorador.getLinha());
+		assertEquals(4, explorador.getColuna());
+	}
+	
+	@Test
+	public void verificaLeituraMapa(){
+		String mapa1 = ". . . .\n"+
+					   ". . . .\n"+
+					   ". . . F\n"+
+					   ". . # R";
+		
+		String mapa2 = ". . . .\n"+
+		   			   ". . . .\n"+
+		   			   ". F B B\n"+
+		   			   "# . . R";
+		
+		Explorador explorador = new Explorador();
+		explorador.definirMapas(mapa1, mapa2);
+		
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
