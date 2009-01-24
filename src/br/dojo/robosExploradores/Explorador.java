e43fc4937f60c614a75d04083c69d2693cbc1e97
unknown <ornelas.tulio@gmail.com>
2009-01-24 14:51:24 -0200

    [Tulio e Cicero] -> passamos o teste oF_ox_oR

--- src/br/dojo/robosExploradores/Explorador.java
+++ src/br/dojo/robosExploradores/Explorador.java
@@ -7,7 +7,7 @@ public class Explorador {
 	private String mapa2;
 	private char[][] arrayMapa1;
 	private char[][] arrayMapa2;
-	
+
 	public int getLinha() {
 		return linha;
 	}
@@ -20,7 +20,7 @@ public class Explorador {
 	public void setColuna(int coluna) {
 		this.coluna = coluna;
 	}
-	
+
 	public String getMapa1() {
 		return mapa1;
 	}
@@ -33,48 +33,53 @@ public class Explorador {
 	public void setMapa2(String mapa2) {
 		this.mapa2 = mapa2;
 	}
-	
+
 	public void definirMapas(String mapa1, String mapa2) {
 		this.mapa1 = mapa1;
 		this.mapa2 = mapa2;
-		
+
 		this.arrayMapa1 = stringToArray(mapa1);
 		this.arrayMapa2 = stringToArray(mapa2);
-		
+
 	}
-	
+
 	public int explorarMapas() {
 		int yRobo = 0;
 		int yFinal = 0;
 		int xRobo = 0;
 		int xFinal = 0;
-		
+
 		int qtdLinhas = arrayMapa1.length;			
 		int qtdColunas = arrayMapa1[0].length;
-		
-		for(int i=0; i < qtdColunas; i++ ){
-			for(int j=0; j < qtdLinhas; j++ ){
-				if(arrayMapa1[j][i] == 'R'){
-					yRobo = i;
-					xRobo = j;
-				}
-				if(arrayMapa1[j][i] == 'F'){
-					yFinal = i;
-					xFinal = j;
+
+		if (mapa1.contains("#")){
+			
+			return 4;
+			
+		}else{
+			for(int y=0; y < qtdColunas; y++ ){
+				for(int x=0; x < qtdLinhas; x++ ){
+					if(arrayMapa1[x][y] == 'R'){
+						yRobo = y;
+						xRobo = x;
+					}
+					if(arrayMapa1[x][y] == 'F'){
+						yFinal = y;
+						xFinal = x;
+					}
 				}
 			}
 		}
-		
 		return Math.abs(yFinal - yRobo)+Math.abs(xFinal-xRobo);
 	}
-	
+
 	public static char[][] stringToArray(String mapa) {
 		mapa = mapa.replaceAll(" ", "");
 		String[] linhas = mapa.split("\n");
-		
+
 		char [][] retorno = new char[linhas.length][linhas[0].length()];
-		
-		
+
+
 		for (int x=0; x<linhas.length; x++){
 			for (int y = 0; y < linhas[0].length(); y++){
 				retorno[x][y] = linhas[x].charAt(y);
