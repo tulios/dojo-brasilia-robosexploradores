781d1ededaf1550f84f268e13943fa1bc7350aee
tulios <ornelas.tulio@gmail.com>
2009-02-07 12:58:59 -0200

    adicionando os novos testes

---
+++ test/br/dojo/robosExploradores/TesteLocomocaoMapasIguaisComObstaculos.java
@@ -0,0 +1,222 @@
+package br.dojo.robosExploradores;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TesteLocomocaoMapasIguaisComObstaculos {
+	
+	@Test
+	public void andarMapa_oF_oX_oR(){
+		String mapa = 	". F \n" +
+						". # \n" +
+						". R \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(4, resultado);
+	}
+
+	@Test
+	public void andarMapa_oF_oX_Ro(){
+		String mapa = 	". F \n" +
+						". # \n" +
+						"R . \n";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(3, resultado);
+	}
+	
+	
+	@Test
+	public void andarMapa_RF_oX_oo(){
+		String mapa = 	"R F \n" +
+						". # \n" +
+						". . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(1, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oF_RX_oo(){
+		String mapa = 	". F \n" +
+						"R # \n" +
+						". . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oF_Xo_Ro(){
+		String mapa = 	"F . \n" +
+						"# . \n" +
+						"R . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(4, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFo_oXo_oRo(){
+		String mapa = 	". F . \n" +
+						". # . \n" +
+						". R . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(4, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFo_oXo_ooR(){
+		String mapa = 	". F . \n" +
+						". # . \n" +
+						". . R \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(3, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFo_oXX_ooR(){
+		String mapa = 	". F . \n" +
+						". # # \n" +
+						". . R \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(5, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFo_XXo_Roo(){
+		String mapa = 	". F . \n" +
+						"# # . \n" +
+						"R . . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(5, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFX_oXo_oRo(){
+		String mapa = 	". F # \n" +
+						". # . \n" +
+						". R . \n";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(4, resultado);
+	}
+	
+	@Test
+	public void andarMapa_XFo_RXo_ooo(){
+		String mapa = 	"# F . \n" +
+						"R # . \n" +
+						". . . \n";
+						
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(6 , resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFX_oXR_ooo(){
+		String mapa = 	". F # \n" +
+						". # R \n" +
+						". . . \n";
+						
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(6 , resultado);
+	}
+	
+	
+	@Test
+	public void andarMapa_oXo_RoF(){
+		String mapa = 	". # . \n" +
+						"R . F \n" ;
+						
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2 , resultado);
+	}
+	
+	@Test
+	public void andarMapa_oXo_FoR(){
+		String mapa = 	". # . \n" +
+						"F . R \n" ;
+						
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2 , resultado);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/br/dojo/robosExploradores/TesteLocomocaoMapasIguaisSemObstaculos.java
@@ -0,0 +1,295 @@
+package br.dojo.robosExploradores;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TesteLocomocaoMapasIguaisSemObstaculos {
+	
+	@Test
+	public void andarMapa_RF(){
+		String mapa = "R F";
+
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(1, resultado);
+	}
+
+	@Test
+	public void andarMapa_RoF(){
+		String mapa = "R . F";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+
+	@Test
+	public void andarMapa_RooF(){
+		String mapa = "R . . F";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(3, resultado);
+	}
+	
+	@Test
+	public void andarMapa_RoooooF(){
+		String mapa = "R . . . . . F";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(6, resultado);
+	}
+
+	@Test
+	public void andarMapa_oRF(){
+		String mapa = ". R F";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(1, resultado);
+	}
+
+	@Test
+	public void andarMapa_oFR(){
+		String mapa = ". F R";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(1, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oFoR(){
+		String mapa = ". F . R";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapa_R_F(){
+		String mapa = 	"R\n" +
+						"F";
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(1, resultado);
+	}
+	
+	@Test
+	public void andarMapa_R_o_F(){
+		String mapa = 	"R\n" +
+						".\n"   +
+						"F";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapa_R_o_o_F(){
+		String mapa = 	"R\n" +
+						".\n" +
+						".\n" +
+						"F";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(3, resultado);
+	}
+	
+	@Test
+	public void andarMapa_R_o_F_o(){
+		String mapa = 	"R\n" +
+						".\n" +
+						"F\n" +
+						".";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapa_F_o_R_o(){
+		String mapa = 	"F\n" +
+						".\n" +
+						"R\n" +
+						".";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapa_F_o_o_o_R_o_o(){
+		String mapa = 	"F\n" +
+						".\n" +
+						".\n" +
+						".\n" +
+						"R\n" +
+						".\n" +
+						".";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(4, resultado);
+	}
+	
+	@Test
+	public void andarMapa_oF_Ro(){
+		String mapa = 	". F\n" +
+						"R .";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapaComplexo(){
+		String mapa = 	". . . . .\n" +
+						". F . . .\n" +
+						". . . . .\n" +
+						". . . . R";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(5, resultado);
+	}
+
+	@Test
+	public void andarMapaComplexo2(){
+		String mapa = 	". . . . .\n" +
+						". F . . .\n" +
+						". . R . .\n" +
+						". . . . .";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void andarMapaComplexo3(){
+		String mapa = 	". . . . .\n" +
+						". R . . .\n" +
+						". . . . .\n" +
+						"F . . . .";
+		
+		
+		Explorador e = new Explorador();
+		e.definirMapas(mapa, mapa);
+		
+		int resultado = e.explorarMapas();
+		assertEquals(3, resultado);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
