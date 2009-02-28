03cd226441969c62b33dd70e0be76187d9e213e7
unknown <Tulio Ornelas@.(none)>
2009-02-28 15:39:12 -0300

    tirando warnings

--- src/br/dojo/robosExploradores/Explorador.java
+++ src/br/dojo/robosExploradores/Explorador.java
@@ -1,10 +1,5 @@
 package br.dojo.robosExploradores;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Explorador {
 	private int linha;
 	private int coluna;
